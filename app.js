b8dca541ed3e08da79221a9819c6b1434e4cd4db
lynnjaco <jacobs.lynnette@gmail.com>
2024-08-21 17:07:26 -0400

    adds route that takes three paramters to build new project name

--- app.js
+++ app.js
@@ -9,5 +9,12 @@ app.get("/", (req, res) => {
   res.send("Welcome to my Express app");
 });
 
+app.get("/:verb/:adjective/:noun", (req, res) => {
+  const { verb, adjective, noun } = req.params;
+  res.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
+  );
+});
+
 // EXPORT
 module.exports = app;
